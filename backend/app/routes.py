f42344fcaca923805b47965e05928a6ef08df42a
shania-codes <sh@ni.a>
2025-11-07 10:48:28 +0000

    simplify backend and reduce scope creep (switch to sqlite instead of postgres since it's planned to be a single user single page application)

--- backend/app/routes.py
+++
@@ -1,6 +0,0 @@
-from app import app
-import time
-
-@app.route('/api/time')
-def get_current_time():
-    return {'time': time.time()}
--- backend/habitgrid.py
+++ backend/habitgrid.py
@@ -1,1 +1,26 @@
-from app import app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import time
+import sqlite3
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+
+db = SQLAlchemy(app)
+
+# DB
+class habit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    description = db.Column(db.String(200))
+
+
+
+with app.app_context():
+    db.create_all()  # creates tables if not exist
+
+# Routes
+@app.route('/api/time')
+def get_current_time():
+    return {'time': time.time()}
